102ed976570807438e99fcc475d4602af2de6e15
George Kolk <2525pala@gmail.com>
2017-11-20 09:42:35 +0200

    add some code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ThreadDemo T1 = new ThreadDemo( "Thread-1");
+        T1.start();
+
+        ThreadDemo T2 = new ThreadDemo( "Thread-2");
+        T2.start();
+
     }
 }
